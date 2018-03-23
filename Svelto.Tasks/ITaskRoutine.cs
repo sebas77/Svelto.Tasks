0a6a40f03aefd55228b3d2edc03f49fc2fd4de42
sebas77 <ssense@tiscali.it>
2018-03-23 17:47:58 +0000

    add comment

--- Svelto.Tasks/ITaskRoutine.cs
+++ Svelto.Tasks/ITaskRoutine.cs
@@ -1,6 +1,25 @@
 using System;
 using System.Collections;
 
+//ITaskRoutine allocated explcitly have several features not 
+//available on task started implicitly with the extension
+//methods.
+
+//TaskRoutine are promises compliant. However the use of Start 
+//and Stop can generate different behaviours
+
+//Start();
+//Stop();
+//Start();
+
+//The new Start will not run immediatly, but will let the Task to stop first
+//and trigger the callback;
+
+//Start();
+//Start();
+
+//allows to start the task immediatly but the OnStop callback won't be triggered
+
 namespace Svelto.Tasks
 {
     public interface ITaskRoutine
